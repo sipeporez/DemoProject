8e0ce5251c276203436b7b1538da897d7b5ef09d
DESKTOP-BRC61UT\USER <sipeporez@gmail.com>
2024-10-22 22:32:16 +0900

    CORS config 수정 - AWS 연동

--- demo/src/main/java/com/example/demo/config/CORSConfig.java
+++ demo/src/main/java/com/example/demo/config/CORSConfig.java
@@ -2,7 +2,6 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,12 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .exposedHeaders(HttpHeaders.AUTHORIZATION)
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedOrigins("http://demoproject-react.s3-website-ap-northeast-1.amazonaws.com");
+
         registry.addMapping("/login")
                 .allowCredentials(true)
                 .exposedHeaders(HttpHeaders.AUTHORIZATION)
-                .allowedMethods(
-                        HttpMethod.POST.name()
-                )
+                .allowedMethods("POST")
                 .allowedOrigins(
                         "http://localhost:8080",
                         "http://localhost:3000"
