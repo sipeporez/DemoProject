d593dc7008115232380d54f33083f70ba06f23a6
DESKTOP-BRC61UT\USER <sipeporez@gmail.com>
2025-02-04 20:33:00 +0900

    유튜브 링크 shorts 추가

--- demo_front/src/Component/Util/YoutubeLinkToTag.js
+++ demo_front/src/Component/Util/YoutubeLinkToTag.js
@@ -1,7 +1,7 @@
 const YoutubeLinkToTag = (e) => {
     // youtube 링크 정규식 
     // html 태그를 포함하여 한줄로 표현되기 때문에 다음 <p> 태그까지 탐색
-    const regex = /(?:https?:\/\/)?(?:www\.|m\.)?(?:youtube\.com|youtu\.be)(?:\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(.*?)(?=<\/p>)/g;
+    const regex = /(?:https?:\/\/)?(?:www\.|m\.)?(?:youtube\.com|youtu\.be)(?:\/(?:[\w\-]+\?v=|embed\/|shorts\/|v\/)?)([\w\-]+)(.*?)(?=<\/p>)/g;
     // 본문 내용 중 일치하는 모든 youtube 링크 매칭
     const matches = [...e.matchAll(regex)];
     let changedLink = e;
@@ -14,7 +14,7 @@ const YoutubeLinkToTag = (e) => {
             return;
         }
         seenVideos.add(videoID); // 비디오 ID 추가
-        let tag = `<iframe width="100%" height="480" src="https://www.youtube.com/embed/${videoID}" frameborder="0" allow="accelerometer; autoplay; picture-in-picture; web-share" allowfullscreen ></iframe></br>`;
+        let tag = `<iframe width="100%" height="480" src="https://www.youtube.com/embed/${videoID}" allowfullscreen="true" referrerpolicy="origin"></iframe></br>`;
         // 매칭된 URL을 iframe으로 교체
         changedLink = changedLink.replace(match[0], tag);
     });
